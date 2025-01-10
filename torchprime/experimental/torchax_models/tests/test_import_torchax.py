29997e70804c56ecbfff01244529b3e99a042202
Yifei Teng <tengyifei@users.noreply.github.com>
2025-01-10 14:31:23 -0800

    Install torch_xla2 in GitHub action checks (#27)
    By default, torch_xla2 is not installed in our testing containers, which
    use the torch_xla docker. This fixes that.
    
    Test: added a test to just import torch_xla2. Run `pytest`.

---
+++ torchprime/experimental/torchax_models/tests/test_import_torchax.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestImportTorchax(unittest.TestCase):
+
+  def test_import_torchax(self):
+    """
+    This test checks that `torchax` is installed.
+    """
+    import torch_xla2  # noqa: F401
