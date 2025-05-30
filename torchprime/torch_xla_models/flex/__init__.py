7a67347f05d8fcca727766f367350107b645cf3c
Weiran Yao <weiran.yao@salesforce.com>
2025-05-29 17:46:37 -0700

    Refactoring Qwen AR to Diff to flex model class (#2)
    * Refactoring
    
    * add qwen for flex to model mapping

--- torchprime/torch_xla_models/flex/__init__.py
+++ torchprime/torch_xla_models/flex/__init__.py
@@ -1,3 +1,5 @@
-from .model import LlamaForCausalLM
+from .modeling_llama import LlamaForCausalLM
+from .modeling_qwen import Qwen3ForCausalLM
 
-__all__ = ["LlamaForCausalLM"]
+
+__all__ = ["LlamaForCausalLM", "Qwen3ForCausalLM"]
