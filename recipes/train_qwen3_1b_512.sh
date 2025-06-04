ed6ffd8e01c79e3930a11bc1d5617e287bf27f51
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2025-06-03 17:08:49 -0700

    Update train_qwen3_1b_512.sh
    test different sharding settings

--- recipes/train_qwen3_1b_512.sh
+++ recipes/train_qwen3_1b_512.sh
@@ -6,9 +6,9 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/test-flex-qwen3-1b-gcs \
     save_steps=20 \
     logging_steps=1 \
-    ici_mesh.fsdp=256 \
+    ici_mesh.fsdp=128 \
     ici_mesh.tensor=1 \
-    ici_mesh.data=1 \
+    ici_mesh.data=2 \
     ici_mesh.expert=1 \
     model/remat=qwen-scan
 # fsdp * tensor * data * expert == global_batch_size
