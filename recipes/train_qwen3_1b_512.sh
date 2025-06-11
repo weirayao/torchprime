c7de398a9ab6bd6ebfc429865bb9929ea70ed1c6
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2025-06-11 01:02:40 -0700

    Update train_qwen3_1b_512.sh

--- recipes/train_qwen3_1b_512.sh
+++ recipes/train_qwen3_1b_512.sh
@@ -3,7 +3,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     model=flex-qwen-1b \
     global_batch_size=512 \
     max_steps=240000 \
-    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex-qwen3-1b-gcs-pretrain-all-data-no-profile \
+    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex-qwen3-1b-gcs-pretrain-all-data-no-profile-1024 \
     save_steps=5000 \
     logging_steps=1 \
     ici_mesh.fsdp=512 \
