dad6d1b988b6c17bf7236fc082ed933db16e70fc
Haolin Chen <haolin.chen@salesforce.com>
2025-06-11 17:29:16 -0700

    update recipe

--- recipes/train_qwen3_1.7b.sh
+++ recipes/train_qwen3_1.7b.sh
@@ -2,10 +2,9 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     data=wikitext \
     model=flex-qwen-1b \
     global_batch_size=8 \
-    max_steps=40 \
-    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/test-flex-qwen3-1b-gcs \
-    resume_from_checkpoint=20 \
-    save_steps=20 \
+    max_steps=20 \
+    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/test-consolidation-flex-qwen3-1b-gcs \
+    save_steps=10 \
     logging_steps=1 \
     ici_mesh.fsdp=8 \
     ici_mesh.tensor=1 \
