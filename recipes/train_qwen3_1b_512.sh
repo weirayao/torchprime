bc5a50bdbd0c314938fe331725a3168b135e407b
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2025-07-07 13:44:55 -0700

    Update train_qwen3_1b_512.sh

--- recipes/train_qwen3_1b_512.sh
+++ recipes/train_qwen3_1b_512.sh
@@ -10,6 +10,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
+    resume_from_checkpoint=9500 \
     model/remat=qwen-scan
 # fsdp * tensor * data * expert == global_batch_size
 # global_batch_size mod num_devices == 0
