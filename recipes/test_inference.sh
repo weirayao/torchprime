6fdd1b5fee0e26c390d4ca26019240731c83ba8d
hlnchen <cn.holiechen@gmail.com>
2025-07-09 18:46:54 +0000

    run on v4-8

--- recipes/test_inference.sh
+++ recipes/test_inference.sh
@@ -1,5 +1,5 @@
 XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/inference.py \
-    global_batch_size=8 \
+    global_batch_size=4 \
     model.tokenizer_name=Qwen/Qwen3-1.7B \
     generation.diffusion_steps=10 \
     generation.max_tokens=0 \
@@ -12,7 +12,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/inference.py \
     generation.return_dict_in_generate=true \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix \
     resume_from_checkpoint=16000 \
-    ici_mesh.fsdp=8 \
+    ici_mesh.fsdp=4 \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
