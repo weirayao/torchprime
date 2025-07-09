c2146315765a22a85db22a0f4e6cdba36113f89d
hlnchen <cn.holiechen@gmail.com>
2025-07-09 00:05:45 +0000

    fix config

--- recipes/ckpt_consolidation.sh
+++ recipes/ckpt_consolidation.sh
@@ -2,7 +2,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/ckpt_consolida
     model=flex-qwen-1b \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix \
     resume_from_checkpoint=13000 \
-    ici_mesh.fsdp=8 \
+    ici_mesh.fsdp=4 \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
