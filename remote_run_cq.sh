4686edeb1c0d98dfd4a1ac0720c4f08174e1574a
Can Qin <cqin@cqin-ltmmh2q.internal.salesforce.com>
2025-07-09 20:48:54 -0700

    revise sft

--- remote_run_cq.sh
+++ remote_run_cq.sh
@@ -2,7 +2,7 @@ TPU_VM_NAME="sfr-cqin-v4-16"
 TPU_ZONE="us-central2-b"
 BRANCH="cqin/dev"
 # RECIPE="recipes/train_qwen3_1.7b.sh"
-RECIPE="recipes/train_qwen3_1.7b_sft_no_flash.sh"
+RECIPE="recipes/train_qwen3_1.7b_sft.sh"
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
