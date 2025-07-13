9583e9ba8ed92320284da6e2785695741623ba86
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2025-07-12 21:55:15 -0700

    Update train.py
    fix the bug that blocks loading huggingface checkpoint.

--- torchprime/torch_xla_models/train.py
+++ torchprime/torch_xla_models/train.py
@@ -695,7 +695,7 @@ def main(config: DictConfig):
   # This will capture the model constructor into a graph so that we can add
   # sharding annotations to the weights later, and run the constructor on the XLA device.
   # NOTE: read HF model from GCS bucket if resume_from_checkpoint is not provided, otherwise read from checkpoint_dir in _load_checkpoint()
-  load_from_checkpoint = hasattr(config, 'resume_from_checkpoint')
+  load_from_checkpoint = hasattr(config, 'resume_from_checkpoint') and config.resume_from_checkpoint is not None
   with set_default_dtype(torch.bfloat16), torch_xla.device():
     model = initialize_model_class(config.model, load_from_hf=not load_from_checkpoint)
 
