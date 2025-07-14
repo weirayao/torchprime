bebf2ebf18d849dc4ddd359780f6e372f019ffcb
Can Qin <cqin@cqin-ltmmh2q.internal.salesforce.com>
2025-07-13 23:25:31 -0700

    update sft config

--- recipes/train_qwen3_1.7b_sft_256core_config.sh
+++ recipes/train_qwen3_1.7b_sft_256core_config.sh
@@ -3,7 +3,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     model=flex-qwen-1b \
     training_mode=sft \
     global_batch_size=256 \
-    max_steps=10000 \
+    max_steps=20000 \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix \
     resume_from_checkpoint=16000 \
     sft_save_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix_sft \
