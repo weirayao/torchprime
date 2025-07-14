154e20bdfc790e17a118b0a4fb31f4257603d8c6
Can Qin <cqin@cqin-ltmmh2q.internal.salesforce.com>
2025-07-14 13:20:29 -0700

    add sft base

---
+++ remote_run_base.sh
@@ -0,0 +1,39 @@
+TPU_VM_NAME="sfr-cqin-v4-16"
+TPU_ZONE="us-central2-b"
+BRANCH="cqin/dev"
+RECIPE="recipes/train_qwen3_1.7b.sh"
+# RECIPE="recipes/train_qwen3_1.7b_sft_256core_config.sh"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -r|--recipe)
+      RECIPE="$2"
+      shift 2
+      ;;
+    -h|--help)
+      echo "Usage: $0 [-r|--recipe RECIPE_PATH]"
+      echo "  -r, --recipe    Path to training recipe (default: recipes/train_qwen3_1.7b.sh)"
+      echo "  -h, --help      Show this help message"
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Use -h or --help for usage information"
+      exit 1
+      ;;
+  esac
+done
+
+gcloud alpha compute tpus tpu-vm ssh $TPU_VM_NAME \
+    --zone=$TPU_ZONE \
+    --project=salesforce-research-internal \
+    --tunnel-through-iap \
+    --worker=all \
+    --command='
+    cd torchprime; \
+    git fetch; \
+    git checkout '"$BRANCH"'; \
+    git pull; \
+    source venv/bin/activate; \
+    bash '"$RECIPE"'';
