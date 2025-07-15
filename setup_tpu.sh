72df8f288ade1993414acfe7b869af0118be8f93
Can Qin <cqin@cqin-ltmmh2q.internal.salesforce.com>
2025-07-15 12:10:39 -0700

    revise config

--- setup_tpu.sh
+++ setup_tpu.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-TPU_VM_NAME="sfr-cqin-v4-128" # Change with your TPU VM name
+TPU_VM_NAME="sfr-cqin-v4-64" # Change with your TPU VM name
 TPU_ZONE="us-central2-b"
 
 # Install python with venv
