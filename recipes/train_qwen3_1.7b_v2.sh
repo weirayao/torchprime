c835f316be7b7c599f13e942ae9905b74759fa86
hlnchen <cn.holiechen@gmail.com>
2025-07-18 22:54:02 +0000

    test masking

--- recipes/train_qwen3_1.7b_v2.sh
+++ recipes/train_qwen3_1.7b_v2.sh
@@ -2,9 +2,9 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     training_mode=pretrain \
     data=mixed_train_dataset \
     model=flex-qwen-1b \
-    model.block_masking_probability=0 \
-    model.truncate_probability=0 \
-    model.prefix_probability=0 \
+    model.block_masking_probability=0.05 \
+    model.truncate_probability=0.01 \
+    model.prefix_probability=0.01 \
     optimizer.learning_rate=3e-4 \
     global_batch_size=512 \
     max_steps=100 \
