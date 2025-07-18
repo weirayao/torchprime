6e66a6c3bbf58032e8a3407d7b6c8d3e148ad695
hlnchen <cn.holiechen@gmail.com>
2025-07-18 06:51:07 +0000

    recipe

--- recipes/train_qwen3_1.7b_v2.sh
+++ recipes/train_qwen3_1.7b_v2.sh
@@ -1,9 +1,11 @@
 XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
+    training_mode=pretrain \
     data=mixed_train_dataset \
     model=flex-qwen-1b \
     model.block_masking_probability=0.01 \
     model.truncate_probability=0.01 \
     model.prefix_probability=0.01 \
+    optimizer.learning_rate=3e-6 \
     global_batch_size=8 \
     max_steps=100 \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/test-masking-flex-qwen3-1b-v2 \
