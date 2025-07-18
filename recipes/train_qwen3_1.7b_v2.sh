392aefc7c6a171b49bae7bbfeb6dc41e07725e29
hlnchen <cn.holiechen@gmail.com>
2025-07-18 22:06:50 +0000

    load ckpt

--- recipes/train_qwen3_1.7b_v2.sh
+++ recipes/train_qwen3_1.7b_v2.sh
@@ -9,7 +9,7 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     global_batch_size=512 \
     max_steps=100 \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/test-masking-flex-qwen3-1b-v2 \
-    resume_from_checkpoint=null \
+    resume_from_checkpoint=50 \
     save_steps=50 \
     logging_steps=1 \
     ici_mesh.fsdp=512 \
