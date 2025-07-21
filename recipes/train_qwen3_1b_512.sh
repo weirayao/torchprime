3b25ade129212c2569f895746bebc8fa1eedd3db
shi-yu-wang <shiyu.wang@salesforce.com>
2025-07-20 21:54:01 -0700

    midtrain

--- recipes/train_qwen3_1b_512.sh
+++ recipes/train_qwen3_1b_512.sh
@@ -1,13 +1,14 @@
-XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
-    data=mixed_train_dataset \
+XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train_mid.py \
+    data=mid_train_dataset \
     model=flex-qwen-1b \
     global_batch_size=256 \
-    max_steps=45000 \
-    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix_from_hf_midtrain \
+    max_steps=30000 \
+    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix_from_hf \
     save_steps=500 \
     logging_steps=1 \
     ici_mesh.fsdp=256 \
-    resume_from_checkpoint=13500 \
+    resume_from_checkpoint=18500 \
+    resume_for_midtrain=True \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
