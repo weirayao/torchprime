78cda84e53734bc9039317f27fdf7f9b890b2aa5
shi-yu-wang <shiyu.wang@salesforce.com>
2025-07-20 21:29:36 -0700

    mid train receipe

--- recipes/train_qwen3_1b_512.sh
+++ recipes/train_qwen3_1b_512.sh
@@ -1,12 +1,12 @@
 XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
     data=mixed_train_dataset \
     model=flex-qwen-1b \
-    global_batch_size=512 \
+    global_batch_size=256 \
     max_steps=45000 \
-    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix_from_hf \
+    checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v1_qw1_7b_512_split_datafix_from_hf_midtrain \
     save_steps=500 \
     logging_steps=1 \
-    ici_mesh.fsdp=512 \
+    ici_mesh.fsdp=256 \
     resume_from_checkpoint=13500 \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
