f7535f220fdff82531c1427fd7626ef5292d3093
shi-yu-wang <shiyu.wang@salesforce.com>
2025-07-22 14:59:16 -0700

    midtrain setup config

--- recipes/train_qwen3_1.7b_midtrain_v2.sh
+++ recipes/train_qwen3_1.7b_midtrain_v2.sh
@@ -1,19 +1,22 @@
-XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train.py \
+XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train_mid.py \
     training_mode=pretrain \
-    data=flex_v2 \
+    data=mid_train_dataset \
     model=flex-qwen-1b \
-    model.block_masking_probability=0.01 \
-    model.mask_block_sizes=[2,4] \
-    model.truncate_probability=0.01 \
-    model.prefix_probability=0.01 \
-    optimizer.learning_rate=3e-4 \
-    global_batch_size=512 \
-    max_steps=50000 \
+    model.block_masking_probability=0.05 \
+    model.mask_block_sizes=[2,4,8] \
+    model.truncate_probability=0.05 \
+    model.prefix_probability=0.05 \
+    optimizer.learning_rate=2e-4 \
+    global_batch_size=256 \
+    max_steps=8000 \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex-qwen3-1b-v2 \
+    checkpoint_dir_for_midtrain=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v2_midtrain \
     resume_from_checkpoint=null \
     save_steps=500 \
     logging_steps=1 \
-    ici_mesh.fsdp=512 \
+    ici_mesh.fsdp=256 \
+    resume_from_checkpoint=42500 \
+    resume_for_midtrain=True \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
