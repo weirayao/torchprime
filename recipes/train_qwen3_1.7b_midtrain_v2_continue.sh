e085f2023e3b2ea6c296a713239e01555cf80ee7
shi-yu-wang <shiyu.wang@salesforce.com>
2025-07-23 14:05:32 -0700

    continue midtrain with bigger blocks

--- recipes/train_qwen3_1.7b_midtrain_v2_continue.sh
+++ recipes/train_qwen3_1.7b_midtrain_v2_continue.sh
@@ -10,11 +10,11 @@ XLA_IR_DEBUG=1 XLA_HLO_DEBUG=1 python torchprime/torch_xla_models/train_mid.py \
     global_batch_size=256 \
     max_steps=15000 \
     checkpoint_dir=gs://sfr-text-diffusion-model-research/checkpoints/flex_processed_v2_midtrain \
-    resume_from_checkpoint=null \
     save_steps=500 \
     logging_steps=1 \
     ici_mesh.fsdp=256 \
     resume_from_checkpoint=7500 \
+    steps_to_skip=7500 \
     ici_mesh.tensor=1 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
