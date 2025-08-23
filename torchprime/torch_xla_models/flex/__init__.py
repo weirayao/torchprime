871038b40f954606eb8e7ad603f69aa0a25259aa
shi-yu-wang <shiyu.wang@salesforce.com>
2025-08-22 19:36:11 -0700

    change __init__ in flex for qwen2

--- torchprime/torch_xla_models/flex/__init__.py
+++ torchprime/torch_xla_models/flex/__init__.py
@@ -1,5 +1,5 @@
 # from .modeling_llama import LlamaForCausalLM
-from .modeling_qwen import Qwen3ForCausalLM
+from .modeling_qwen2 import Qwen2ForCausalLM
 
 
-__all__ = ["Qwen3ForCausalLM"]
+__all__ = ["Qwen2ForCausalLM"]
