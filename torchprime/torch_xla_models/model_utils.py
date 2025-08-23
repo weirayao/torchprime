bac3e44497451edf464cbe5cf4eaaada6a010441
shi-yu-wang <shiyu.wang@salesforce.com>
2025-08-23 14:43:36 -0700

    device_map=cpu

--- torchprime/torch_xla_models/model_utils.py
+++ torchprime/torch_xla_models/model_utils.py
@@ -206,7 +206,7 @@ def load_hf_model(model_config):
   hf_model = hf_model_class.from_pretrained(
     model_config.tokenizer_name,
     torch_dtype=torch.bfloat16,
-    device_map="auto",
+    device_map="cpu",
   )
   return hf_model
 
