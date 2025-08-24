453e8f2a9f7412db765cca13ba5317aab1ae5cfb
hlnchen <cn.holiechen@gmail.com>
2025-08-24 05:57:15 +0000

    revert train

--- torchprime/torch_xla_models/train.py
+++ torchprime/torch_xla_models/train.py
@@ -636,9 +636,8 @@ class Trainer:
     else:
       # Pre-training mode (original behavior)
       _logits, loss = self.model(
-        input_ids=batch["input_ids"],
-        labels=batch["input_ids"],
-        # masking_schedule=masking_schedule
+        **batch,
+        masking_schedule=masking_schedule
       )
     loss.backward()
     self.optimizer.step()
