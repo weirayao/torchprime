f78deb4ea2b1aa8ed8e0f4b7c3d94807dc5bdf01
shi-yu-wang <shiyu.wang@salesforce.com>
2025-08-24 22:02:59 -0700

    add Qwen2ForCausalLM to flex

--- torchprime/torch_xla_models/flex/__init__.py
+++ torchprime/torch_xla_models/flex/__init__.py
@@ -1,5 +1,6 @@
 # from .modeling_llama import LlamaForCausalLM
 from .modeling_qwen import Qwen3ForCausalLM
+from .modeling_qwen2 import Qwen2ForCausalLM
 
 
 __all__ = ["Qwen3ForCausalLM", "Qwen2ForCausalLM"]
