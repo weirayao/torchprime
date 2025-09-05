5a18774cb8ac34bd4a5ed223a7ff6934a64d23b8
hlnchen <cn.holiechen@gmail.com>
2025-09-05 17:26:53 +0000

    remove non streaming

--- torchprime/data/dataset.py
+++ torchprime/data/dataset.py
@@ -78,9 +78,7 @@ def make_gcs_pretokenized_dataset(
   data = load_dataset(
     "parquet",
     data_files=data_files,
-    streaming=False,
-    keep_in_memory=True,
-    num_proc=96,
+    streaming=True,
     split="train",
   )
   if checkpoint_dir is not None and is_main_process():
