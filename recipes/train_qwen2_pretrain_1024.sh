ca5dd68c35fc0831e5cd41e3866b124f653aa4f6
hlnchen <cn.holiechen@gmail.com>
2025-09-06 05:19:56 +0000

    recipe

--- recipes/train_qwen2_pretrain_1024.sh
+++ recipes/train_qwen2_pretrain_1024.sh
@@ -23,8 +23,8 @@ python torchprime/torch_xla_models/train.py \
     checkpoint_save_dir=gs://sfr-text-diffusion-model-research/checkpoints/pretrain_qwen2_5_Coder_1_5b_flex_v2_segment_attn/ \
     save_steps=500 \
     logging_steps=1 \
-    ici_mesh.fsdp=256 \
-    ici_mesh.tensor=2 \
+    ici_mesh.fsdp=128 \
+    ici_mesh.tensor=4 \
     ici_mesh.data=1 \
     ici_mesh.expert=1 \
     model/remat=qwen2-scan
