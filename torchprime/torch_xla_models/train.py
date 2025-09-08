2bc01589c4b30338576a17c8cad89a199125d52c
hlnchen <cn.holiechen@gmail.com>
2025-09-08 01:49:20 +0000

    debug

--- torchprime/torch_xla_models/train.py
+++ torchprime/torch_xla_models/train.py
@@ -301,9 +301,9 @@ class Trainer:
         self.train_dataset,
         collate_fn=webdataset_collate_fn,
         batch_size=per_worker_batch_size,
-        num_workers=4,
+        num_workers=2,
         persistent_workers=True,
-        prefetch_factor=4,
+        prefetch_factor=2,
         pin_memory=False,
         drop_last=True,
       )
