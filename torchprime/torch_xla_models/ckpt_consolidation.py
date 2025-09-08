f9294573ce9de08ae31994d4cb288e4baea79b09
hlnchen <cn.holiechen@gmail.com>
2025-09-08 18:00:10 +0000

    debug

--- torchprime/torch_xla_models/ckpt_consolidation.py
+++ torchprime/torch_xla_models/ckpt_consolidation.py
@@ -125,7 +125,7 @@ def main(config: DictConfig):
       
       reload_sd = {
         "model": {
-          name.replace("._orig_mod", ""): torch.empty(tensor.shape, dtype=tensor.dtype, device="cpu")
+          name: torch.empty(tensor.shape, dtype=tensor.dtype, device="cpu")
           for name, tensor in model_sd.items()
         }
       }
