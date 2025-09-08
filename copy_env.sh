b1e8be751a1ce1d8c38e0486e7b075e7b1f9073d
hlnchen <cn.holiechen@gmail.com>
2025-09-08 02:28:37 +0000

    script

--- copy_env.sh
+++ copy_env.sh
@@ -1,5 +1,5 @@
-TPU_VM_NAME="sfr-haolin-chen-v4-1024"
-TPU_ZONE="us-central2-b"
+TPU_VM_NAME="sfr-haolin-chen-v5p-1024"
+TPU_ZONE="us-central1-a"
 
 # Copy .env file to TPU VM
 gcloud alpha compute tpus tpu-vm scp .env $TPU_VM_NAME:~/torchprime/ \
