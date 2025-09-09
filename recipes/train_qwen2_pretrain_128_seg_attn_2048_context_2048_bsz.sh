cf273d52340c5fb08f5634bccf023e4a05349984
shi-yu-wang <shiyu.wang@salesforce.com>
2025-09-08 19:46:25 -0700

    large bsz set att 2048 seq

--- recipes/train_qwen2_pretrain_128_seg_attn_2048_context_2048_bsz.sh
+++ recipes/train_qwen2_pretrain_128_seg_attn_2048_context_2048_bsz.sh
@@ -11,9 +11,9 @@ export GCSFS_DEFAULT_FILL_CACHE=false
 # export PT_XLA_DEBUG_LEVEL=2
 # export HYDRA_FULL_ERROR=1
 python torchprime/torch_xla_models/train.py \
-    run_name=context_8192_segment_attn_batch_2048_lr_3e-4 \
+    run_name=reshape_context_2048_segment_attn_large_batch_2048_lr_3e-4 \
     training_mode=pretrain \
-    reshape_context=false \
+    reshape_context=true \
     seg_attn=true \
     data=flex_v2_webdataset \
     model=flex-qwen2-1b \
