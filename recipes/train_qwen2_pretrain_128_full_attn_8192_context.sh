aa843532952632da92c460c513dfdf7dbcdae907
hlnchen <cn.holiechen@gmail.com>
2025-09-09 01:21:07 +0000

    fix recipe

--- recipes/train_qwen2_pretrain_128_full_attn_8192_context.sh
+++ recipes/train_qwen2_pretrain_128_full_attn_8192_context.sh
@@ -11,7 +11,7 @@ export GCSFS_DEFAULT_FILL_CACHE=false
 # export PT_XLA_DEBUG_LEVEL=2
 # export HYDRA_FULL_ERROR=1
 python torchprime/torch_xla_models/train.py \
-    run_name=\'context 8192, full attn, small batch, lr 3e-4\' \
+    run_name="context 8192, full attn, small batch, lr 3e-4" \
     training_mode=pretrain \
     reshape_context=false \
     seg_attn=false \
