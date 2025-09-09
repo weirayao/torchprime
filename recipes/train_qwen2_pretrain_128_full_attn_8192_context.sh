61c5d21fa2250fb7c530301d2c1cb18af6f8cc6a
hlnchen <cn.holiechen@gmail.com>
2025-09-09 17:10:28 +0000

    continue recipe

--- recipes/train_qwen2_pretrain_128_full_attn_8192_context.sh
+++ recipes/train_qwen2_pretrain_128_full_attn_8192_context.sh
@@ -11,7 +11,7 @@ export GCSFS_DEFAULT_FILL_CACHE=false
 # export PT_XLA_DEBUG_LEVEL=2
 # export HYDRA_FULL_ERROR=1
 python torchprime/torch_xla_models/train.py \
-    run_name=context_8192_full_attn_small_batch_lr_3e-4 \
+    run_name=context_8192_full_attn_small_batch_lr_3e-4_continued \
     training_mode=pretrain \
     reshape_context=false \
     seg_attn=false \
@@ -28,9 +28,9 @@ python torchprime/torch_xla_models/train.py \
     lr_scheduler.warmup_steps=250 \
     global_batch_size=512 \
     max_steps=210000 \
-    checkpoint_load_dir=null \
-    checkpoint_load_step=null \
-    resume_from_checkpoint=false \
+    checkpoint_load_dir=gs://sfr-text-diffusion-model-research/checkpoints/pretrain_qwen25_coder_1b_flex_v2_context_8192_full_attn_bsz_512_lr_3e-4/ \
+    checkpoint_load_step=10000 \
+    resume_from_checkpoint=true \
     checkpoint_save_dir=gs://sfr-text-diffusion-model-research/checkpoints/pretrain_qwen25_coder_1b_flex_v2_context_8192_full_attn_bsz_512_lr_3e-4/ \
     save_steps=10000 \
     logging_steps=1 \
