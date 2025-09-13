7e5ef8973233d1f640bb3c56504ccc79525698ad
hlnchen <cn.holiechen@gmail.com>
2025-09-13 07:23:56 +0000

    validation

--- recipes/validation.sh
+++ recipes/validation.sh
@@ -10,25 +10,24 @@ export GCSFS_DEFAULT_FILL_CACHE=false
 
 # export PT_XLA_DEBUG_LEVEL=2
 # export HYDRA_FULL_ERROR=1
-checkpoints=(1000 2000 3000 4000 5000 6000 7000)
-for checkpoint in "${checkpoints[@]}"; do
-    python torchprime/torch_xla_models/pretrain_validation.py \
-        run_name=validation_midtrain_qwen25_coder_1b_flex_v2_mask0_20_256_context_8192_seg_attn_bsz_2048_lr_1e-4_${checkpoint} \
-        training_mode=pretrain \
-        reshape_context=false \
-        seg_attn=true \
-        data=validation \
-        model=flex-qwen2-1b \
-        global_batch_size=2048 \
-        max_steps=150 \
-        checkpoint_load_dir=gs://sfr-text-diffusion-model-research/checkpoints/midtrain_qwen25_coder_1b_flex_v2_mask0_20_256_context_8192_seg_attn_bsz_2048_lr_1e-4 \
-        checkpoint_load_step=${checkpoint} \
-        resume_from_checkpoint=false \
-        logging_steps=1 \
-        ici_mesh.fsdp=32 \
-        ici_mesh.tensor=2 \
-        ici_mesh.data=1 \
-        ici_mesh.expert=1 \
-        model/remat=qwen2-scan
-done
+
+python torchprime/torch_xla_models/pretrain_validation.py \
+    run_name=validation_midtrain_qwen25_coder_1b_flex_v2_mask0_20_256_context_8192_seg_attn_bsz_2048_lr_1e-4_${checkpoint} \
+    training_mode=pretrain \
+    reshape_context=false \
+    seg_attn=true \
+    data=validation \
+    model=flex-qwen2-1b \
+    global_batch_size=2048 \
+    max_steps=150 \
+    checkpoint_load_dir=gs://sfr-text-diffusion-model-research/checkpoints/midtrain_qwen25_coder_1b_flex_v2_mask0_20_256_context_8192_seg_attn_bsz_2048_lr_1e-4 \
+    checkpoint_load_step=1000 \
+    resume_from_checkpoint=false \
+    logging_steps=1 \
+    ici_mesh.fsdp=32 \
+    ici_mesh.tensor=2 \
+    ici_mesh.data=1 \
+    ici_mesh.expert=1 \
+    model/remat=qwen2-scan
+
 # fsdp * tensor * data * expert == num_devices
