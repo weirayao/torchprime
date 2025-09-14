8091390f2427fdee72f7da99cc6525d55b661c67
hlnchen <cn.holiechen@gmail.com>
2025-09-14 00:20:40 +0000

    fix typo

--- torchprime/torch_xla_models/train.py
+++ torchprime/torch_xla_models/train.py
@@ -217,7 +217,7 @@ class Trainer:
     if checkpoint_load_step in tracked_steps:
       if is_main_process():
         logger.info(f"Loading checkpoint from step {checkpoint_load_step}")
-      if config.model.model_class == "flex.Qwen2ForCausalLM":
+      if self.config.model.model_class == "flex.Qwen2ForCausalLM":
         load_dir = os.path.join(MOUNTED_GCS_DIR, self.checkpoint_load_dir.split(GCS_PREFIX)[1], f"unsharded_state_dict_{checkpoint_load_step}.pt")
         unsharded_state_dict = torch.load(load_dir)
         self.model.load_state_dict(unsharded_state_dict, strict=False)
@@ -227,7 +227,7 @@ class Trainer:
       last_step = max(tracked_steps)
       if is_main_process():
         logger.warning(f"Checkpoint step {checkpoint_load_step} not found in tracked steps {tracked_steps}. Loading from latest checkpoint {last_step}.")
-      if config.model.model_class == "flex.Qwen2ForCausalLM":
+      if self.config.model.model_class == "flex.Qwen2ForCausalLM":
         load_dir = os.path.join(MOUNTED_GCS_DIR, self.checkpoint_load_dir.split(GCS_PREFIX)[1], f"unsharded_state_dict_{checkpoint_load_step}.pt")
         unsharded_state_dict = torch.load(load_dir)
         self.model.load_state_dict(unsharded_state_dict, strict=False)
